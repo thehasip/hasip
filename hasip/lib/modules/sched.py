2222f15699b1f3f4f869a12576f4ca7af9ebf8c8
Michael Hoedl <michael.hoedl@edu.fh-kaernten.ac.at>
2014-01-06 23:44:25 +0100

    corrected statement in rest.md set up base structure for scheduler module

--- hasip/lib/modules/sched.py
+++ hasip/lib/modules/sched.py
@@ -2,7 +2,7 @@ from lib.base.modules import *
 import threading
 import time
 from apscheduler.scheduler import Scheduler
-
+import logging
 
 class Sched():
 
@@ -13,23 +13,17 @@ class Sched():
     #
     # "sched|port|command or action"
     #
-
+    self.logger = logging.getLogger('Hasip.sched')
     self.sched = Scheduler()
     self.sched.start()
     self.queue_identifier = 'sched'       # this is the 'module address'  
     self.instance_queue = instance_queue  # worker queue to receive jobs 
     self.global_queue = global_queue      # queue to communicate back to main thread
-    self.ports = [                        # internal port names
-      { 
-        'id'      : 0,
-        'status'  : 'on'
-      }, { 
-        'id'      : 1,
-        'type'    : 'pump',
-        'status'  : 'off'
-      }, {}, {}, {} ] # defining internal ports here (...)
-
     
+    
+    # @TODO loading jobs from persistent store and create them in the scheduler
+
+
   # ################################################################################
   # main thread of this module file which runs in background and constanly
   # checks working queue for new tasks. 
@@ -40,13 +34,13 @@ class Sched():
         instance_queue_element = self.instance_queue.get()
 
         _action = instance_queue_element.get("cmd")
-        _port   = instance_queue_element.get("module_id")
+        _opt_args   = instance_queue_element.get("opt_args")
 
         options = {
-          "set_off"   : self.set_off,
-          "pumptest"  : self.pumptest
+          "create"   : self.create,
+          "delete"  : self.delete
         }
-        options[_action](_port)
+        options[_action](_opt_args)
 
   # ################################################################################
   #
@@ -54,54 +48,13 @@ class Sched():
   #
   # ################################################################################
 
-  
-  # ################################################################################
-  # sets the port provided by @agrument to off
-  #
-  # @arguments:  port
-  # @return:     -
-  # ################################################################################
-  def set_off(self, port):
-    self.ports[port]['status'] = 'off'
-    self.sched.stop()
-    print "Scheduler :: set_off(" + str(port) + ")"
+  def create(opt_args)
+    # @TODO
+    print "Function to put jobs in the running scheduler job queue and store them persistent"
+    pass
 
-  # ################################################################################
-  # Testing method to switch pump off and on for Demonstration purpose
-  # sets a interval scheduler that switches off the GPIO pin 3 off and on via the
-  # GPIO module. Pump should be attached to that port.
-  # @arguments:  port
-  # @return:     -
-  # ################################################################################
-  def pumptest(self, port):
-    print "Pumptest started"
-    
-    queue_msg_start = {
-        'module':       'gpio',
-        'module_id':    0,
-        'cmd':          'set_on',
-        'opt_args':     ''
-    }
-    queue_msg_stop = {
-        'module':       'gpio',
-        'module_id':    0,
-        'cmd':          'set_off',
-        'opt_args':     ''
-    }
+  def delete(opt_args)
+    # @TODO
+    print "Function to delete running and persistent jobs"
+    pass
 
-    start = self.global_queue.put(queue_msg_start)
-    stop = self.global_queue.put(queue_msg_stop)
-    self.sched.add_interval_job(lambda: start, minutes=0.2)
-    self.sched.add_interval_job(lambda: stop, minutes=0.05)
-    print "Pumptest stopped"
-
-#
-# main for testing
-#
-#if __name__ == '__main__':
-#  import Queue
-#  q = Queue.Queue()
-#  h = Sched(q,q)
-#  h.pumptest(0)
-#  
-# h.worker()
