15a5db454f10984fb2bcb375b3cc97b2cd350f37
Michael Hoedl <michael.hoedl@edu.fh-kaernten.ac.at>
2014-01-06 23:27:23 +0100

    solved merging conflict in __init__.py

--- hasip/lib/modules/__init__.py
+++ hasip/lib/modules/__init__.py
@@ -2,7 +2,5 @@
 from lib.modules.cmddemo import Cmddemo
 from lib.modules.sched import Sched
 from lib.modules.gpio import Gpio
-<<<<<<< HEAD
-=======
 from lib.modules.rest import Rest
->>>>>>> 403c5df99d68d0341da53d1411a5540e59dc50a6
+
